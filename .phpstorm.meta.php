2ab90402c900913fe1dd3f3b58dbbe25cf17fd5e
Charlie Steenhagen <csteen1005@gmail.com>
2020-05-09 23:05:31 -0400

    Added no scripts to composer install

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-05-10 02:08:43.
+    * Generated on 2020-05-10 03:01:53.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.8.37 on 2020-05-10 02:08:42.
+ * Generated for Laravel 5.8.37 on 2020-05-10 03:01:52.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
