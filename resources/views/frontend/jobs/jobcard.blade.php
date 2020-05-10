18e02350407a8c48c2791517885128623750bc7c
Charlie Steenhagen <csteen1005@gmail.com>
2020-05-10 13:22:30 -0400

    Changed the style on the job card a little

--- resources/views/frontend/jobs/jobcard.blade.php
+++ resources/views/frontend/jobs/jobcard.blade.php
@@ -12,9 +12,13 @@
                 <img src="{{$job->images()->first() ? Storage::url($job->images()->first()->small_url) : ""}}" class="job-image">
             </div>
             <div class="col-12 col-md-6 m-auto {{$order == 'even' ? 'order-md-12' : 'order-md-1'}} ">
-                <h1 class="card-title">{{$job->title}}</h1>
-                <h4 class="card-subtitle mb-2 text-muted">{{$job->company}}</h4>
-                <h6 class="card-subtitle mb-2 text-secondary">{{date_format(date_create($job->started_at), 'm-d-Y')}} - {{$job->finished_at == "" ? "Present" : date_format(date_create($job->finished_at), 'm-d-Y')}}</h6>
+                <h2 class="card-title">{{$job->title}}</h2>
+                <h5 class="card-subtitle mb-2 text-muted">{{$job->company}}</h5>
+                <h6 class="card-subtitle mb-2 text-secondary">
+                    <small>
+                        {{date_format(date_create($job->started_at), 'm-d-Y')}} - {{$job->finished_at == "" ? "Present" : date_format(date_create($job->finished_at), 'm-d-Y')}}
+                    </small>
+                </h6>
                 <p class="card-text">{{$job->short_description}}</p>
             </div>
         </div>
