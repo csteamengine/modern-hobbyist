ea5acc4b5c9f775fa71b5a63e69ebd54caf09212
Charlie Steenhagen <csteen1005@gmail.com>
2020-05-12 19:53:42 -0400

    Trying https redirect

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -35,7 +35,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if(config('app.env') === 'production'){
+        if(config('app.env') !== 'dev'){
             URL::forceScheme('https');
         }
 
