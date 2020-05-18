b8797a3f7d641d42fb1296bbe2df71a0f7c754c7
Charlie Steenhagen <csteen1005@gmail.com>
2020-05-18 07:52:47 -0400

    Not sure what I changed but its working now

--- public/js/backend/projects/projects.js
+++ public/js/backend/projects/projects.js
@@ -8,7 +8,9 @@
 /***/ (function(module, exports) {
 
 $(document).ready(function () {
-  $('#projectsTable').DataTable();
+  $('#projectsTable').DataTable({
+    "order": [[2, "desc"]]
+  });
   var reset = false;
 });
 
@@ -27,4 +29,4 @@ module.exports = __webpack_require__(/*! /Users/gregorysteenhagen/Desktop/Modern
 /***/ })
 
 },[[7,"/js/manifest"]]]);
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9yZXNvdXJjZXMvanMvYmFja2VuZC9wcm9qZWN0cy9wcm9qZWN0cy5qcyJdLCJuYW1lcyI6WyIkIiwiZG9jdW1lbnQiLCJyZWFkeSIsIkRhdGFUYWJsZSIsInJlc2V0Il0sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7QUFBQUEsQ0FBQyxDQUFDQyxRQUFELENBQUQsQ0FBWUMsS0FBWixDQUFrQixZQUFZO0FBQzFCRixHQUFDLENBQUMsZ0JBQUQsQ0FBRCxDQUFvQkcsU0FBcEI7QUFDQSxNQUFJQyxLQUFLLEdBQUcsS0FBWjtBQUNILENBSEQsRSIsImZpbGUiOiIvanMvYmFja2VuZC9wcm9qZWN0cy9wcm9qZWN0cy5qcyIsInNvdXJjZXNDb250ZW50IjpbIiQoZG9jdW1lbnQpLnJlYWR5KGZ1bmN0aW9uICgpIHtcbiAgICAkKCcjcHJvamVjdHNUYWJsZScpLkRhdGFUYWJsZSgpO1xuICAgIHZhciByZXNldCA9IGZhbHNlO1xufSk7XG5cbiJdLCJzb3VyY2VSb290IjoiIn0=
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9yZXNvdXJjZXMvanMvYmFja2VuZC9wcm9qZWN0cy9wcm9qZWN0cy5qcyJdLCJuYW1lcyI6WyIkIiwiZG9jdW1lbnQiLCJyZWFkeSIsIkRhdGFUYWJsZSIsInJlc2V0Il0sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7QUFBQUEsQ0FBQyxDQUFDQyxRQUFELENBQUQsQ0FBWUMsS0FBWixDQUFrQixZQUFZO0FBQzFCRixHQUFDLENBQUMsZ0JBQUQsQ0FBRCxDQUFvQkcsU0FBcEIsQ0FBK0I7QUFDM0IsYUFBUyxDQUFDLENBQUUsQ0FBRixFQUFLLE1BQUwsQ0FBRDtBQURrQixHQUEvQjtBQUdBLE1BQUlDLEtBQUssR0FBRyxLQUFaO0FBQ0gsQ0FMRCxFIiwiZmlsZSI6Ii9qcy9iYWNrZW5kL3Byb2plY3RzL3Byb2plY3RzLmpzIiwic291cmNlc0NvbnRlbnQiOlsiJChkb2N1bWVudCkucmVhZHkoZnVuY3Rpb24gKCkge1xuICAgICQoJyNwcm9qZWN0c1RhYmxlJykuRGF0YVRhYmxlKCB7XG4gICAgICAgIFwib3JkZXJcIjogW1sgMiwgXCJkZXNjXCIgXV1cbiAgICB9ICk7XG4gICAgdmFyIHJlc2V0ID0gZmFsc2U7XG59KTtcblxuIl0sInNvdXJjZVJvb3QiOiIifQ==
