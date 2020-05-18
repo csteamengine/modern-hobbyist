c9b803442216171dc7a241f1bf11367f52847af1
Charlie Steenhagen <csteen1005@gmail.com>
2020-05-17 21:33:16 -0400

    Added access control origin headers

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -41,6 +41,10 @@ $app->singleton(
     App\Exceptions\Handler::class
 );
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: *');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With,Authorization, Content-Type, Accept');
+
 /*
 |--------------------------------------------------------------------------
 | Return The Application
