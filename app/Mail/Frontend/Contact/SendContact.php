49ddae49a61d72497eb87ddfbf6cff43583a5c4a
Charlie Steenhagen <csteen1005@gmail.com>
2020-09-30 07:50:05 -0400

    Update SendContact.php

--- app/Mail/Frontend/Contact/SendContact.php
+++ app/Mail/Frontend/Contact/SendContact.php
@@ -39,7 +39,7 @@ class SendContact extends Mailable
         return $this->to(get_active_profile()->contact_email, config('mail.from.name'))
             ->view('frontend.mail.contact')
             ->text('frontend.mail.contact-text')
-            ->subject(__('strings.emails.contact.subject', ['app_name' => app_name()])." from ".$this->request->name)
+            ->subject(__('strings.emails.contact.subject', ['app_name' => app_name(), 'from_name' => $this->request->name]))
             ->from(config('mail.from.address'), $this->request->name)
             ->replyTo($this->request->email, $this->request->name);
     }
